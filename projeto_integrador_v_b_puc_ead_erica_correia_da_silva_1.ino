c337dc15bae9f5aced776adde5505f8cca7da1bd
ericacs <125032502+ericacs@users.noreply.github.com>
2025-04-06 16:42:00 -0300

    Update projeto_integrador_v_b_puc_ead_erica_correia_da_silva_1.ino

--- projeto_integrador_v_b_puc_ead_erica_correia_da_silva_1.ino
+++ projeto_integrador_v_b_puc_ead_erica_correia_da_silva_1.ino
@@ -44,45 +44,53 @@ void loop() {
   luminosidadePercentual = (luminosidadeValue / 1023.0) * 100; // Conversão para percentual
 
   // Exibindo os valores no monitor serial
+  /*
   Serial.print("Temperatura (TMP36): ");
-  Serial.print(temperatureC);   // Leitura do TMP36
+  Serial.println("Temperatura:"+temperatureC);   // Leitura do TMP36
   Serial.print("\tUmidade (Potenciometro): ");
-  Serial.print(umidadePercentual);      // Leitura do Potenciometro (Umidade) em percentual
+  Serial.println("Umidade"+umidadePercentual);      // Leitura do Potenciometro (Umidade) em percentual
   Serial.print("\tLuminosidade (Potenciometro): ");
-  Serial.println(luminosidadePercentual); // Leitura do Potenciometro (Luminosidade) em percentual
-
-  // Imprimir o valor de luminosidade para depuracao
-  Serial.print("Valor de luminosidade (bruto): ");
+  Serial.println("Luminosidade"+luminosidadePercentual); // Leitura do Potenciometro (Luminosidade) em percentual
+  */
+  
+  //Enviando para porta serial em formato tabular ex: temp|umid|lumn 
+  Serial.print(temperatureC);  
+  Serial.print("|");
+  Serial.print(+umidadePercentual);
+  Serial.print("|");
   Serial.println(luminosidadePercentual);
+  
+  // Imprimir o valor de luminosidade para depuracao
+  //Serial.print("Valor de luminosidade (bruto): ");
+  //Serial.println(luminosidadePercentual);
 
   // Controle do ventilador (Motor CC) baseado na temperatura
   if (temperatureC >= 23) {
     digitalWrite(ventiladorPin, HIGH);  // Liga o ventilador (motor gira)
-    Serial.println("Ventilador ligado"); // Exibe no monitor serial
+    //Serial.println("Ventilador ligado"); // Exibe no monitor serial
   } else {
     digitalWrite(ventiladorPin, LOW);   // Desliga o ventilador (motor para)
-    Serial.println("Ventilador desligado"); // Exibe no monitor serial
+    //Serial.println("Ventilador desligado"); // Exibe no monitor serial
   }
 
   // Controle da lampada (Luminosidade)
   // A lampada deve acender quando a luminosidade for baixa (<50%)
   if (luminosidadePercentual < 50) {  // Ajuste este valor conforme necessário
     digitalWrite(lampadaPin, HIGH);  // Liga a lampada
-    Serial.println("Lampada ligada"); // Exibe no monitor serial
+    //Serial.println("Lampada ligada"); // Exibe no monitor serial
   } else {
     digitalWrite(lampadaPin, LOW);   // Desliga a lampada
-    Serial.println("Lampada desligada"); // Exibe no monitor serial
+    //Serial.println("Lampada desligada"); // Exibe no monitor serial
   }
 
   // Controle de umidade (LED de umidade)
   if (umidadePercentual >= 50) {  // Ajuste o limite de umidade conforme necessário
     digitalWrite(ledUmidadePin, HIGH);  // Acende o LED para indicar alta umidade
-    Serial.println("Led ligado"); // Exibe no monitor serial
+    //Serial.println("Led ligado"); // Exibe no monitor serial
   } else {
     digitalWrite(ledUmidadePin, LOW);   // Desliga o LED para indicar baixa umidade
-    Serial.println("Led desligado"); // Exibe no monitor serial
+    //Serial.println("Led desligado"); // Exibe no monitor serial
   }
-
-  // Atraso de 5,5 segundo antes da próxima leitura
-  delay(5500);
+  // Atraso de 3 segundo antes da próxima leitura
+  delay(3000);
 }
